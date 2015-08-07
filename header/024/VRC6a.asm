5c03f7c842542a66f3b0ca0a2de70d3df89e9ba5
freem <aj187@ajworld.net>
2015-08-07 01:10:28 -0500

    notes in the two VRC6 variants

--- header/024/VRC6a.asm
+++ header/024/VRC6a.asm
@@ -1,4 +1,8 @@
-; VRC6a:
+; Konami VRC6a: 256KB PRG-ROM + 8KB PRG-RAM + 256KB CHR-ROM (iNES Mapper 024)
 ; http://bootgod.dyndns.org:7777/profile.php?id=3316
 ; (Akumajou Densetsu)
 ;------------------------------------------------------------------------------;
+; This is the version of the VRC6 where the register addresses are in order:
+; $x000, $x001, $x002, $x003
+
+
