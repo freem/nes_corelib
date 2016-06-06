0fc7d541801aa2a1a7981ae90a98fd15f0880905
freem <aj187@ajworld.net>
2016-06-06 17:41:11 -0500

    update link to magic floor (actual commit of substance compared to the upcoming one)

--- header/218/SingleChip.asm
+++ header/218/SingleChip.asm
@@ -1,6 +1,6 @@
 ; Single Chip Cartridge
 ; http://forums.nesdev.com/viewtopic.php?t=9342
-; Used by Magic Floor: http://nocash.emubase.de/magicflr.htm
+; Used by Magic Floor: http://problemkaputt.de/magicflr.htm
 ;------------------------------------------------------------------------------;
 ; This one is interesting; it doesn't have any CHR-ROM or CHR-RAM. Instead, it
 ; uses the internal 2KB Nametable RAM as CHR-RAM.
@@ -13,7 +13,7 @@
 ; 1Scr. B	%1001
 MIRRORING = %1001
 
-; Mapper 218 (PRG chip-only; CIC if required) iNES v1.0 header
+; Mapper 218 (PRG chip-only; CIC if required) iNES header
 	.byte "NES",$1A
 	.byte $01				; 1x 16K PRG banks
 	.byte $00				; Using nametable ram as CHR-RAM
