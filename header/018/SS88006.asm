bb750b49bda3dffb332027bd3cba2d5c1ab9dafe
freem <aj187@ajworld.net>
2025-11-29 23:33:45 -0600

    remove all pre-made headers; `nes2_header.asm` should be used instead

--- header/018/SS88006.asm
+++
@@ -1,3 +0,0 @@
-; Jaleco SS880066: (128-256)KB PRG-ROM + (128-256?)KB CHR-ROM
-; http://bootgod.dyndns.org:7777/search.php?keywords=18&kwtype=pcb
-;------------------------------------------------------------------------------;
--- header/026/VRC6b.asm
+++
@@ -1,6 +0,0 @@
-; Konami VRC6b: 256KB PRG-ROM + 8KB PRG-RAM + 256KB CHR-ROM (iNES Mapper 024)
-; http://bootgod.dyndns.org:7777/search.php?keywords=26&kwtype=pcb
-; (Mouryou Senki Madara and Esper Dream 2)
-;------------------------------------------------------------------------------;
-; This is the version of the VRC6 where the two middle register addresses are
-; swapped: $x000, $x002, $x001, $x003
